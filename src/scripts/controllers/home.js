e6e6aa11358239a912b899857d77ac6fe6ecb6ff
Max Mulatz <klappradla@gmail.com>
2015-07-15 23:11:31 +0200

    add news section

--- src/scripts/controllers/home.js
+++ src/scripts/controllers/home.js
@@ -2,12 +2,12 @@
 
 angular.module('tanzmobil')
   .controller('HomeCtrl', function ($scope, WpService) {
-
-    // WpService.post('zwei-eulen').then(function(response) {
-    //   console.log(response);
-    // });
-    WpService.recentPosts(2).then(function(response) {
+    WpService.recentPosts(2).then(function (response) {
       console.log(response);
       $scope.posts = response;
     });
+
+    WpService.page('news').then(function (response) {
+      $scope.news = response;
+    })
   });
