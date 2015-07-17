b5363880aa4f3a5e4ae61e86a99f895bcd7e5770
Max Mulatz <klappradla@gmail.com>
2015-07-17 18:11:29 +0200

    styling search form

--- src/scripts/directives/searchform.js
+++ src/scripts/directives/searchform.js
@@ -1,15 +1,30 @@
 'use strict';
 angular.module('tanzmobil')
-  .directive('searchForm', function(CONFIG) {
+  .directive('searchForm', function(CONFIG, $timeout) {
     return {
       restrict: 'E',
       replace: true,
       templateUrl: CONFIG.ROOT_URL + 'assets/views/components/searchform.html',
       link: function(scope, element) {
-        console.log(scope);
+
         scope.submitSearch = function() {
-          console.log(scope.searchInput);
-          scope.search(scope.searchInput);
+          if (!scope.showSearchInput) {
+            scope.showSearchInput = true;
+            $timeout(function() {
+              element.find('input')[0].focus(); 
+            });
+          } else if (!scope.searchInput) {
+            scope.showSearchInput = false;
+          } else {
+            console.log(scope.searchInput);
+            scope.search(scope.searchInput);
+          }
+        };
+
+        scope.searchInputBlur = function() {
+          $timeout(function() {
+            scope.showSearchInput = false;
+          });
         };
       }
     }
