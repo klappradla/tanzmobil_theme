ebdfa51156b178643988789499bd7015d487fa08
Max Mulatz <klappradla@gmail.com>
2015-08-02 16:34:25 +0200

    add interviewteaser placeholder image

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -292,9 +292,21 @@ angular.module('tanzmobil')
   });'use strict';
 angular.module('tanzmobil')
   .directive('interviewTeaser', function(CONFIG) {
+
+    function teaserImage(post) {
+      if (post.thumbnail_images) {
+        return post.thumbnail_images.full.url
+      }
+      return CONFIG.ROOT_URL + 'assets/images/placeholder.jpg'
+    }
+
     return {
       restrict: 'E',
-      templateUrl: CONFIG.ROOT_URL + 'assets/views/components/interviewteaser.html'
+      replace: true,
+      templateUrl: CONFIG.ROOT_URL + 'assets/views/components/interviewteaser.html',
+      link: function(scope, element) {
+        scope.teaserImage = teaserImage(scope.post);
+      }
     }
   });'use strict';
 angular.module('tanzmobil')
