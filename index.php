5a8d12bf0f88d8ecc7c383e51ee735d7ab698bf8
Max Mulatz <klappradla@gmail.com>
2015-08-02 16:11:43 +0200

    add google analytics

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<title>Tanzmobil Production</title>
+	<title>tanzmobil</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
   <base href="/">
@@ -28,5 +28,17 @@
   </footer>
 
 	<?php wp_footer();?>
+
+  <!-- start google -->
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-61874302-1', 'auto');
+    ga('send', 'pageview');
+  </script>
+  <!-- end google -->
 </body>
 </html>
